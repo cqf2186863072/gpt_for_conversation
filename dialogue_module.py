2e7796c6eb163bcd19e4ebabdd69acd7d9f23d44
cqf2186863072 <2186863072@qq.com>
2023-08-05 18:06:25 +0800

    add saving dialogue function

--- dialogue_module.py
+++ dialogue_module.py
@@ -1,7 +1,10 @@
 import re
+import os
+import configparser
+import json
+from datetime import datetime
 from enum import Enum
 from gpt_module import GPTClient
-
 
 class InputMode(Enum):
     KEYBOARD = 0
@@ -11,57 +14,85 @@ class OutputMode(Enum):
     PRINT = 0
     SPEAK = 1
 
+class CheckResult(Enum):
+    BREAK = 0
+    CONTINUE = 1
+
 class Message:
-    '''This is a class that orgnize a message'''
     def __init__(self, role, content):
         self.role = role
         self.content = content
 
-
-    def __dic__(self):
-        '''This is a method that converts the message role and content into a dictionary'''
+    def to_dict(self):
         return {"role": f"{self.role}", "content": self.content}
 
 class DialogueManager:
     def __init__(self, gpt_client, synthesizer=None, recognizer=None):
-        '''This is the constructor of the class
-
-        Args:
-        gpt_client: A GPTClient object that communicates with the GPT-4-32k API
-        synthesizer: A Synthesizer object that converts text to speech (optional)
-        recognizer: A Recognizer object that converts speech to text (optional)
-        
-        '''
         self.gpt_client = gpt_client
         self.synthesizer = synthesizer
         self.recognizer = recognizer
-        self.set_prompts()
-        
+        self.set_prompt()
+        self.init_keywords()
 
-        # 配置对话模式和指令词
-        #TODO 给指令一个特殊的格式
+    def init_keywords(self):
         self.InputMode = InputMode.KEYBOARD
         self.OutputMode = OutputMode.PRINT
+        self.save_dialogue_words = ["保存", "save"]
+        self.save_dialogue_pattern = self.build_pattern(self.save_dialogue_words)
         self.multiline_words = ["多行", "multiline"]
-        self.multiline_pattern = "|".join([f"^({word})[。.]?\s*$" for word in self.multiline_words])
+        self.multiline_pattern = self.build_pattern(self.multiline_words)
         self.switch_output_words = ["切换输出模式", "语音"]
-        self.switch_output_pattern = "|".join([f"^({word})[。.]?\s*$" for word in self.switch_output_words])
+        self.switch_output_pattern = self.build_pattern(self.switch_output_words)
         self.switch_input_words = ["切换输入模式", "切换", "switch", "change", "toggle"]
-        self.switch_input_pattern = "|".join([f"^({word})[。.]?\s*$" for word in self.switch_input_words])
+        self.switch_input_pattern = self.build_pattern(self.switch_input_words)
         self.exit_words = ["退出", "结束", "停止", "exit", "end", "quit"]
-        self.exit_pattern = "|".join([f"^({word})[。.]?\s*$" for word in self.exit_words])
+        self.exit_pattern = self.build_pattern(self.exit_words)
 
-    #TODO 配置对话历史的方法
-    def set_prompts(self):
-        self.dialogue_history = [{"role": "system", "content": ''}]
+    def build_pattern(self, words):
+        return "|".join([f"^({word})[。.]?\s*$" for word in words])
+
+    def set_prompt(self, file_name=None):
+        '''Method to set prompt and send diologue history
+        
+        请在调用run方法前调用该方法
+
+        '''
+        if file_name:
+            try:
+                file_name = os.path.join('saved_dialogue_history', file_name)
+                if os.path.exists(file_name):
+                    # with open(file_name, 'r', encoding='utf-8') as file:
+                    with open(file_name, 'r') as file:
+                        self.dialogue_history = json.load(file)
+                    print("prompt初始化成功")
+                else:
+                    raise Exception(f'未找到文件{file_name}')
+            except Exception as e:
+                print(e)
+        else:
+            self.dialogue_history = [{"role": "system", "content": ''}]
+
+    def save_dialogue(self):
+        file_name = input("请输入要保存的文件名：")
+        if not file_name:
+            file_name = datetime.now().strftime("%Y%m%d_%H%M%S")  # 默认命名为日期+时间
+
+        # 创建'saved_dialogue_history'文件夹
+        if not os.path.exists('saved_dialogue_history'):
+            os.makedirs('saved_dialogue_history')
+
+        # 将对话记录保存为json文件
+        with open(os.path.join('saved_dialogue_history', file_name + '.json'), 'w') as file:
+            json.dump(self.dialogue_history, file)
+
+        # 将对话记录保存为txt文件
+        with open(os.path.join('saved_dialogue_history', file_name + '.txt'), 'w', encoding='utf-8') as file:
+            for message in self.dialogue_history:
+                file.write(f"{message['role']}: {message['content']}\n")
+
+        print("对话记录已保存")
 
     def get_multiline_input(self):
-        '''This is a method that gets multiple lines of input from the user
-
-        Return:
-            A string that contains the user input with newline characters
-    
-        '''    
         print("请输入多行内容，以END结束")
         lines = []
         while True:
@@ -70,7 +101,7 @@ class DialogueManager:
                 break
             lines.append(line)
         return "\n".join(lines)
-    
+
     def output(self, text):
         if self.synthesizer and self.OutputMode == OutputMode.SPEAK:
             self.synthesizer.text_to_speech_speaker_async(text)
@@ -98,35 +129,48 @@ class DialogueManager:
             self.OutputMode = OutputMode.PRINT
             print("已切换到文本输出模式，请输入")
 
+    def check_key_words(self):
+        self.CheckResult = None
+
+        if re.match(self.save_dialogue_pattern, self.user_input):
+            self.save_dialogue()
+            self.CheckResult = CheckResult.CONTINUE
+        if re.match(self.switch_output_pattern, self.user_input):
+            self.switch_OutputMode()
+            self.CheckResult = CheckResult.CONTINUE
+        if re.match(self.switch_input_pattern, self.user_input):
+            self.switch_InputMode()
+            self.CheckResult = CheckResult.CONTINUE
+        if re.match(self.multiline_pattern, self.user_input):
+            self.user_input = self.get_multiline_input()
+
+        if re.match(self.exit_pattern, self.user_input):
+            self.exit_flag = True
+            self.CheckResult = CheckResult.BREAK
+
     def run(self):
         self.exit_flag = False
         while not self.exit_flag:
             try:
                 if self.InputMode == InputMode.VOICE:
                     print("User: ")
-                    user_input = f"{self.recognizer.speech_to_text_microphone()}"
-                    print(user_input)
+                    self.user_input = f"{self.recognizer.speech_to_text_microphone()}"
+                    print(self.user_input)
                 else:
-                    user_input = input("User: ")
+                    self.user_input = input("User: ")
 
-                if re.match(self.multiline_pattern, user_input):
-                    user_input = self.get_multiline_input()
-                if re.match(self.switch_output_pattern, user_input):
-                    self.switch_OutputMode()
-                    continue
-                if re.match(self.switch_input_pattern, user_input):
-                    self.switch_InputMode()
-                    continue
-                if re.match(self.exit_pattern, user_input):
-                    self.exit_flag = True
+                self.check_key_words()
+                if self.CheckResult == CheckResult.BREAK:
                     break
+                elif self.CheckResult == CheckResult.CONTINUE:
+                    continue
 
-                user_message = Message("user", user_input)
-                self.dialogue_history.append(user_message.__dic__())
+                user_message = Message("user", self.user_input)
+                self.dialogue_history.append(user_message.to_dict())
                 text = self.gpt_client.send_message(self.dialogue_history)
                 self.output(text)
-                system_message = Message("system", text)
-                self.dialogue_history.append(system_message.__dic__())
+                assistant_message = Message("assistant", text)
+                self.dialogue_history.append(assistant_message.to_dict())
 
             except Exception as e:
                 print(f"Error: {e}")
@@ -135,8 +179,24 @@ class DialogueManager:
 
 
 if __name__ == '__main__':
-    url = "https://gpt-4-32k-api.com/" # replace with your own url
-    header = {"Authorization": "Bearer xxx"} # replace with your own header
+    # 用配置文件配置gpt
+    config = configparser.ConfigParser()
+    file_dir = os.path.dirname(__file__)
+    config_file = os.path.join(file_dir, 'config.ini')
+    if os.path.exists(config_file): 
+        config.read(config_file)
+        url = config.get('gpt', 'url')
+        header = eval(config.get('gpt', 'header'))
+    else:
+        print("请配置config.ini")
     gpt_client = GPTClient(url, header)
+
+    # 手动配置gpt
+    # url = "https://gpt-4-32k-api.com/" # replace with your own url
+    # header = {"Authorization": "Bearer xxx"} # replace with your own header
+    # gpt_client = GPTClient(url, header)
+
+    # 运行对话系统
     dialogue_manager = DialogueManager(gpt_client)
+    dialogue_manager.set_prompt('编程高手.json')
     dialogue_manager.run()
